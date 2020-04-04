867a6484cbc1bd1b68a06464dda40137cd431f7f
jsous7 <43439510+jsous7@users.noreply.github.com>
2020-04-04 04:04:18 +0100

    Creating method for displaying the menu

--- src/juliana/cct/oodp/EntryPoint.java
+++ src/juliana/cct/oodp/EntryPoint.java
@@ -10,8 +10,23 @@ package juliana.cct.oodp;
  * @author julia
  */
 public class EntryPoint {
-    
+
     public static void main(String[] args) {
-        System.out.println("Test");
+        displayMenu();
+
     }
+
+    private static void displayMenu() {
+        System.out.println("Select one option:\n");
+
+        System.out.println("1.Create a country");
+        System.out.println("2.Show all countries");
+        System.out.println("3.Show country details by name");
+        System.out.println("4.Show country details by code");
+        System.out.println("5.Update country");
+        System.out.println("6.Delete country");
+        System.out.println("7.Exit");
+
+    }
+
 }
