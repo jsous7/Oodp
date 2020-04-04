7fa9fdefb4f64a27d4b08ffd8e62fc8b554cad0e
jsous7 <43439510+jsous7@users.noreply.github.com>
2020-04-04 03:41:36 +0100

    Initial Commit

---
+++ src/juliana/cct/oodp/EntryPoint.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juliana.cct.oodp;
+
+/**
+ *
+ * @author julia
+ */
+public class EntryPoint {
+    
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
