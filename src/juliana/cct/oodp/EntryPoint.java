b704e58065f142f0f4b2396c31c1855db766e44f
jsous7 <43439510+jsous7@users.noreply.github.com>
2020-04-04 05:42:50 +0100

    User input logic for main menu

--- src/juliana/cct/oodp/EntryPoint.java
+++ src/juliana/cct/oodp/EntryPoint.java
@@ -5,20 +5,29 @@
  */
 package juliana.cct.oodp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author julia
  */
 public class EntryPoint {
 
-    public static void main(String[] args) {
-        displayMenu();
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static boolean stopLoop = false;
 
+    public static void main(String[] args) throws IOException {
+        while (!stopLoop) {
+            displayMenu();
+            String optionSelected = bufferedReader.readLine();
+            mainLogic(optionSelected);
+        }
     }
-
+    
     private static void displayMenu() {
         System.out.println("Select one option:\n");
-
         System.out.println("1.Create a country");
         System.out.println("2.Show all countries");
         System.out.println("3.Show country details by name");
@@ -26,7 +35,36 @@ public class EntryPoint {
         System.out.println("5.Update country");
         System.out.println("6.Delete country");
         System.out.println("7.Exit");
-
     }
 
+    private static void mainLogic(String option) throws IOException {
+        switch (option) {
+            case "1":
+                //TODO
+                break;
+            case "2":
+                System.out.println("Option selected!");
+                System.out.println("Press any key to continue...");
+                bufferedReader.readLine();
+                break;
+            case "3":
+                //TODO
+                break;
+            case "4":
+                //TODO
+                break;
+            case "5":
+                //TODO
+                break;
+            case "6":
+                //TODO
+                break;
+            case "7":
+                stopLoop = true;
+                break;
+            default:
+                System.out.println("\"" + option + "\" Is an invalid option ");
+                break;
+        }
+    }
 }
